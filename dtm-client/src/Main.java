2fa70ce40eb88fce9fd224463c5a1745e5ce6832
ShySodium <120010274+ShySodium@users.noreply.github.com>
2024-06-04 21:14:51 +0200

    Added client split of the main project code
    Based on the pre-GUI files, re-add whatever's necessary

---
+++ dtm-client/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        String address;
+
+        // Keep asking the user for server address
+        RemoteHostMasterThread hostThread = null;
+        do {
+            try
+            {
+                System.out.print("Connect to: ");
+                address = in.next();
+
+                InetAddress serverAddress = InetAddress.getByName(address);
+                //System.out.println(serverAddress);
+                hostThread = new RemoteHostMasterThread(serverAddress, 51707);
+                break;
+            }
+            catch (ConnectException connectionRefused)
+            {
+                System.out.println("Connection refused, bad address or server is closed");
+            }
+        } while(true);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.submit(hostThread);
+    }
+}
